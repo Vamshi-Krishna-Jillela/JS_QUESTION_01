1c1cf065e76bb2de02d4de4f9d5f22b2e586a10c
Vamshi-Krishna-Jillela <89860169+Vamshi-Krishna-Jillela@users.noreply.github.com>
2021-11-01 19:00:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+var x="hello world.\nJavascript file can be linked the using then script tag in html page.\nThe file location should be copied to src attribute in the script tag.\nThis can be done in both header tag and body tag";
+console.log(x);
